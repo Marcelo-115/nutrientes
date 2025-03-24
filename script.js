b0de2548792fa996d5369560b41c9d827a026d57
Marcelo-115 <108548027+Marcelo-115@users.noreply.github.com>
2025-03-24 15:29:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function calcularNutrientes(){
+	let peso = parseFloat(document.getElementById('peso').value);
+
+		if(isNaN(peso)||peso <= 0 ){
+		alert('Insira um peso valido!')
+		return;
+
+		}
+
+	let agua = peso * 0.035;
+	let copos = agua / 0.2;
+	let proteina = peso * 2;
+	let creatina = peso * 0.03;
+
+
+	document.getElementById('agua').innerText = `Consumo de Agua: ${agua.toFixed(1)} ml por dia`;
+	document.getElementById('coposDaguas').innerText = `Equivante a ${copos.toFixed(0)} copos de agua de 200 ml por dia`;
+	document.getElementById('proteina').innerText = `Consumo de proteina: ${proteina.toFixed(0)} g por dia`;
+	document.getElementById('creatina').innerText = `Consumo de creatina: ${creatina.toFixed(0)} g por dia`;
+
+
+
+}
